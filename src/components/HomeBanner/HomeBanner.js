18621ba1a132427823c3db569668cbeed4d085fb
ishil17 <129098013+ishil17@users.noreply.github.com>
2024-10-08 00:08:19 +0530

    Add files via upload

---
+++ src/components/HomeBanner/HomeBanner.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./HomeBanner.css";
+
+const HomeBanner = () => {
+	return (
+		<div className="cardBanner">
+			<div className="card">
+				
+				<div className="card-img-overlay">
+					<div className="banner_body">
+						<div className="row">
+							<div className="col-sm-12 col-md-6 col-lg-6 offset-sm-0 offset-md-3 offset-lg-6">
+								
+							
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default HomeBanner;
